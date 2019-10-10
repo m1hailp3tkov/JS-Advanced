c0b5bb2dfc5721df63b4e0a6c9f9a817765ebd83
m1hailp3tkov <41004048+m1hailp3tkov@users.noreply.github.com>
2019-10-10 13:08:39 +0300

    Update solution.js

--- hw_dom/07. Shopping-Cart/solution.js
+++ hw_dom/07. Shopping-Cart/solution.js
@@ -1,3 +1,40 @@
-function solve() {
-   //TODO...
+function solve() 
+{
+   let addButtons = [...document.querySelectorAll('.add-product')];
+   addButtons.map(x => x.addEventListener('click', e => addProduct(e)));
+
+   let checkoutButton = document.querySelector("body > div > button");
+   checkoutButton.addEventListener('click', checkout);
+
+   let textArea = document.querySelector("body > div > textarea");
+   
+   let uniqueProducts = [];
+   let totalPrice = 0;
+
+   function addProduct(e)
+   {
+      let productDiv = e.target.parentNode.parentNode;
+
+      let productName = productDiv.getElementsByClassName('.product-title')[0].innerHtml;
+      let productCost = productDiv.getElementsByClassName('.product-line-price')[0].innerHtml;
+
+      if(!uniqueProducts.includes(productName)) 
+      {
+         uniqueProducts.push(productName);
+      }
+
+      textArea.textContent += `Added ${productName} for ${productCost} to the cart.\n`;
+      totalPrice += Number(productCost);
+   }
+
+   function checkout()
+   {
+      totalPrice = Math.round(totalPrice * 100) / 100;
+      let list = uniqueProducts.join(', '); 
+
+      textArea.textContent += `You bought ${list} for ${totalPrice}.`;
+
+      addButtons.map(x => x.disabled = true);
+      checkoutButton.disabled = true;
+   }
 }
